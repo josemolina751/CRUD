6bc165073f2c84e71b5288cb9afc502aeed8993a
josemolina751 <joseantoniomolinatrabajo@gmail.com>
2025-11-13 19:41:23 +0100

    update with Hibernate

--- src/main/java/com/example/Product.java
+++ src/main/java/com/example/Product.java
@@ -16,19 +16,6 @@ public class Product {
     private String category;
     private String description;
 
-    public Product(int id, String name, double price, int stock, String category, String description) {
-
-        //CONSTRUCTOR EXISTENTE
-
-        this.id = id;
-        this.name = name;
-        this.price = price;
-        this.stock = stock;
-        this.category = category;
-        this.description = description;
-
-    }
-
     public Product() {
 
     }
@@ -49,44 +36,8 @@ public class Product {
         this.id = id;
     }
 
-    public String getName() {
-        return name;
-    }
-
     public void setName(String name) {
         this.name = name;
-    }
-
-    public double getPrice() {
-        return price;
-    }
-
-    public void setPrice(double price) {
-        this.price = price;
-    }
-
-    public int getStock() {
-        return stock;
-    }
-
-    public void setStock(int stock) {
-        this.stock = stock;
-    }
-
-    public String getCategory() {
-        return category;
-    }
-
-    public void setCategory(String category) {
-        this.category = category;
-    }
-
-    public String getDescription() {
-        return description;
-    }
-
-    public void setDescription(String description) {
-        this.description = description;
     }
 
     @Override
